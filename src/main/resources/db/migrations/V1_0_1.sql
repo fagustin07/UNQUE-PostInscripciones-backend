792f4ed32dfd2f7b912e7920eb3616caf7146d7f
Federico Sandoval <federico.sandoval@alu.unq.edu.ar>
2022-06-10 12:07:55 -0300

    :bug: correccion de ddl

--- src/main/resources/db/migrations/V1_0_1.sql
+++ src/main/resources/db/migrations/V1_0_1.sql
@@ -1,17 +1,26 @@
+CREATE TABLE directivo
+(
+    correo      VARCHAR(255) NOT NULL,
+    nombre      VARCHAR(255) NULL,
+    contrasenia VARCHAR(255) NULL,
+    rol         VARCHAR(255) NULL,
+    CONSTRAINT pk_directivo PRIMARY KEY (correo)
+);
+
 ALTER TABLE cuatrimestre
-    MODIFY anio INT NULL;
+    ALTER anio INT NULL;
 
 ALTER TABLE alumno
-    MODIFY coeficiente DOUBLE NULL;
+    ADD coeficiente DOUBLE NULL;
 
 ALTER TABLE comision
-    MODIFY cupos_totales INT NULL;
+    ALTER cupos_totales INT NULL;
 
 ALTER TABLE comision
-    MODIFY numero INT NULL;
+    ALTER numero INT NULL;
 
 ALTER TABLE comision
-    MODIFY sobrecupos_ocupados INT NULL;
+    ALTER sobrecupos_ocupados INT NULL;
 
 ALTER TABLE comision
-    MODIFY sobrecupos_totales INT NULL;
+    ALTER sobrecupos_totales INT NULL;
